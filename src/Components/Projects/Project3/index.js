7078d3e4d830103e8a62f28262b299b4b83f4d0c
xanadel <trevinwoods41@gmail.com>
2021-11-07 16:11:50 -0600

    finished projects 1-3 on portfolio page

--- src/Components/Projects/Project3/index.js
+++ src/Components/Projects/Project3/index.js
@@ -4,18 +4,20 @@ export default function ProjectThreeComponent() {
 
     return (
         <div className="projectThree_outmost_ctnr">
-            <div className="projectTwo_SS_ctnr">
-                <img className="projectTwo splash" src={process.env.PUBLIC_URL + "/Gamebrite/Splash.png"} alt="" />
-                <img className="projectTwo eventView" src={process.env.PUBLIC_URL + "/Gamebrite/ViewEvent.png"} alt="" />
-                <img className="projectTwo userEvents" src={process.env.PUBLIC_URL + "/Gamebrite/MyEvents.png"} alt="" />
+            <div className="projectThree_SS_ctnr">
+                <img className="projectThree splash" src={process.env.PUBLIC_URL + "/Kickstarter/splash.png"} alt="" />
+                <img className="projectThree project" src={process.env.PUBLIC_URL + "/Kickstarter/ProjectView.png"} alt="" />
+                <img className="projectThree updates" src={process.env.PUBLIC_URL + "/Kickstarter/UpdatesView.png"} alt="" />
+                <img className="projectThree faqs" src={process.env.PUBLIC_URL + "/Kickstarter/FAQView.png"} alt="" />
+                <img className="projectThree comments" src={process.env.PUBLIC_URL + "/Kickstarter/CommentsView.png"} alt="" />
             </div>
-            <p className="projectTwo_swipeLabel">Swipe to view more</p>
-            <p className="projectTwo_title" >Gamebrite</p>
-            <p className="projectTwo_description" > An Eventbrite clone inspired by my love of video games. This was built using Javascript and Express communicating with a PostgreSQL database for the backend. This was my first attempt at styling on my own. I consider this my most challenging application with HTML and CSS because there was a lot to learn. This application also uses React and Redux for the frontend. </p>
-            <div className="projectTwo_links_ctnr">
-                <a className="projectTwo_live_link" href="https://triple-confirmed-games.herokuapp.com/">Live </a>
+            <p className="projectThree_swipeLabel">Swipe to view more</p>
+            <p className="projectThree_title" >Kickstarter</p>
+            <p className="projectThree_description" > A Kickstarter clone, most effort was put into making it an exact clone. This was built using Python and Flask communicating with a PostgreSQL database for the backend. This application also uses React and Redux for the frontend with alot of moving bits and pieces to make this a high quality clone. HTML5 and CSS3 were used to bring the application to life with almost pixel perfect styling </p>
+            <div className="projectThree_links_ctnr">
+                <a className="projectThree_live_link" href="https://triple-confirmed-games.herokuapp.com/">Live </a>
                 {'|'}
-                <a className="projectTwo_repo_link" href="https://github.com/Woods-Trevin/TripleConfirmedGames"> Repo</a>
+                <a className="projectThree_repo_link" href="https://github.com/Woods-Trevin/TripleConfirmedGames"> Repo</a>
             </div>
         </div>
     );
--- src/Components/Projects/Project4/index.js
+++ src/Components/Projects/Project4/index.js
@@ -4,7 +4,28 @@ export default function ProjectFourComponent() {
 
     return (
         <div className="projectFour_outmost_ctnr">
-            <h1> Project Four Component</h1>
+            <div className="projectFour_SS_ctnr">
+                <img className="projectFour splashTop" src={process.env.PUBLIC_URL + "/ChurchConnect/TopSplash.png"} alt="" />
+                <img className="projectFour splashBottom" src={process.env.PUBLIC_URL + "/ChurchConnect/BottomSplash.png"} alt="" />
+                <img className="projectFour createAnnouncement" src={process.env.PUBLIC_URL + "/ChurchConnect/CreateAnnouncement.png"} alt="" />
+                <img className="projectFour createEvent" src={process.env.PUBLIC_URL + "/ChurchConnect/CreateEvent.png"} alt="" />
+                <img className="projectFour display" src={process.env.PUBLIC_URL + "/ChurchConnect/DisplayPage.png"} alt="" />
+            </div>
+            <p className="projectFour_swipeLabel">Swipe to view more</p>
+            <p className="projectFour_title" >Kickstarter</p>
+            <p className="projectFour_description" >
+                ChurchConnect was a pleasure to design. This was the first project I was able to make
+                without any help with backend calls or frontend redux state.
+                This application uses Flask and Python communicating with a PostgreSQL database for the backend.
+                The frontend uses React Components and Hooks and utilizes Redux to store data from the backend
+                in useful slices of state. HTML and CSS were used to design an application that was pleasing to
+                the eyes and easy to use for all ages.
+            </p>
+            <div className="projectFour_links_ctnr">
+                <a className="projectFour_live_link" href="https://triple-confirmed-games.herokuapp.com/">Live </a>
+                {'|'}
+                <a className="projectFour_repo_link" href="https://github.com/Woods-Trevin/TripleConfirmedGames"> Repo</a>
+            </div>
         </div>
     );
 };
